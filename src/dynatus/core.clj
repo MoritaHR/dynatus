eb2c0cc3897a2d686c34278978aae1f41373a86f
Rafael Campo <rafael@nearshoredev.com>
2025-12-01 15:52:05 -0500

    defn syncronize -> handle empty dir / replace migrate with syncronize fn
    cljfmt fix
    
    Throw exception when table is not active after 5 attempts
    
    clojure fmt fix
    
    Fix tests
    
    remove test
    
    Fix tests

--- src/dynatus/core.clj
+++ src/dynatus/core.clj
@@ -9,7 +9,7 @@
   [client table-name]
   (println "Waiting for table" table-name "to become active...")
   (loop [attempts 0]
-    (when (< attempts 30) ; Max 30 attempts (30 seconds)
+    (when (< attempts 5) ; Max 5 attempts (5 seconds)
       (Thread/sleep 1000) ; Wait first to give table time to be created
       (let [result (aws/invoke client {:op :DescribeTable
                                        :request {:TableName table-name}})
@@ -25,18 +25,41 @@
           :else
           (recur (inc attempts)))))))
 
-(defn migrate
+(defn execute-tables-sync
+  [client defs]
+  (mapv
+   (fn [definition]
+     (let [table-name (:TableName definition)
+           actual     (loader/describe-table client table-name)
+           d          (diff/compute definition actual)]
+       (apply/apply-diff client d)
+       (when (#{:create :recreate} (:action d))
+         (wait-for-table-active client table-name))
+       ;; Return the diff so the caller knows what happened
+       {:table table-name
+        :action (:action d)}))
+   defs))
+
+(defn syncronizate
   "Run migrations for DynamoDB.
    Options:
    {:client <aws-client>
     :path   \"resources/dynamo\"}"
   [{:keys [client path]}]
-  (let [defs (loader/load-definitions path)]
-    (doseq [definition defs]
-      (let [table-name (:TableName definition)
-            actual     (loader/describe-table client table-name)
-            d          (diff/compute definition actual)]
-        (apply/apply-diff client d)
-        ;; Always wait after any action that creates/modifies a table
-        (when (#{:create :recreate} (:action d))
-          (wait-for-table-active client table-name))))))
+  (let [path (or path "./resources/dynamo")
+        defs (loader/load-definitions path)]
+    (try
+      (if (empty? defs)
+        {:sync false
+         :reason "No DynamoDB migration files found."
+         :path path}
+
+        (let [results (execute-tables-sync client defs)]
+          {:sync true
+           :migrated results
+           :count (count results)}))
+
+      (catch Exception e
+        {:sync false
+         :reason "Exception while executing DynamoDB sync."
+         :error (.getMessage e)}))))
